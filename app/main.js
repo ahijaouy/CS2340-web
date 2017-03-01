521ff6f411a56222f41ea2a6b4c8fc827cf191be
Oberon <ahijaouy@gatech.edu>
2017-03-01 17:20:18 -0500

    basic page navigation

--- app/main.js
+++ app/main.js
@@ -1,7 +1,32 @@
 module.exports = function(app) {
 
+
+	app.get('/logout', function(req, res) {
+		res.send("logout called");
+	});
+
+	app.get('/profile', function(req, res) {
+		res.send('Editting Profile...');
+	});
+
 	app.get('/', function(req, res) {
 		res.render('index');
+	});
+
+	app.get('/index', function(req, res) {
+		res.render('index');
+	});
+
+	app.get('/water_reports', function(req, res) {
+		res.render('water_reports');
+	});
+
+	app.get('/historical_reports', function(req, res) {
+		res.render('historical_reports');
+	});
+
+	app.get('/admin', function(req, res) {
+		res.render('admin');
 	});
 
 };
